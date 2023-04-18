bcb362ab8285cae4db5ffdcb73441a40a04ebb22
Konstantin8881888 <kamagoroff@gmail.com>
2023-04-18 16:50:29 +0300

    Initial Spring

---
+++ src/test/java/com/example/answer7/Answer7ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.answer7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Answer7ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
