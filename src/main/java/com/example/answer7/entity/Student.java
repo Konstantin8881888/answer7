53dd0d36bfe4da3065b07358188ec0da990ec029
Konstantin8881888 <kamagoroff@gmail.com>
2023-04-20 22:00:48 +0300

    current update

---
+++ src/main/java/com/example/answer7/entity/Student.java
@@ -0,0 +1,22 @@
+package com.example.answer7.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "student")
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "age")
+    private int age;
+}
