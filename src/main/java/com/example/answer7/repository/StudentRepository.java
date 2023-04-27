80a0e17d3d1a65d5262cf2381fe3c0e3b28a3af4
Konstantin8881888 <kamagoroff@gmail.com>
2023-04-27 17:48:19 +0300

    Current Changes

---
+++ src/main/java/com/example/answer7/repository/StudentRepository.java
@@ -0,0 +1,9 @@
+package com.example.answer7.repository;
+
+import com.example.answer7.entity.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StudentRepository extends JpaRepository<Student, Long> {
+}
